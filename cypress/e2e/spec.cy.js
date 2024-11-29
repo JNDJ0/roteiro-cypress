fb1c9af08883c5cec121a994458effbd4ffeea90
joao.moreira <joao.moreira@sydle.com>
2024-11-29 19:29:27 -0300

    feat: adicionando testes faltantes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,67 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Completa todas as tarefas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Primeira tarefa{enter}')
+      .type('Segunda tarefa{enter}')
+
+    cy.get('.todo-list li')
+      .should('have.length', 2);
+    
+    cy.get('.toggle-all-label')
+      .click();
+    cy.get('.clear-completed')
+      .click();
+    
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+    
+  });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('');
+
+    cy.get('.new-todo')
+      .type('TP3 de Engenharia de software{enter}');
+
+    cy.get('.todo-list li')
+      .dblclick();
+
+    cy.get('.todo-list li .edit')
+      .clear()
+      .type('TP2 de Engenharia de software{enter}');
+
+    cy.get('.todo-list li')
+      .first()
+      .should('have.text', 'TP2 de Engenharia de software');
+  });
+
+  it('Concluir uma tarefa', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de ES{enter}')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('[data-cy=filter-completed-link')
+      .click();
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 1)
+      .first()
+      .should('have.text', 'TP2 de ES');
+
+    cy.get('[data-cy=filter-active-link')
+      .click();
+    cy.get('.todo-list li')
+      .should('have.length', 0);
+
+  });
 });
